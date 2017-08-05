04238c4284f86cae502796d17c64ab3768fbd653
Roshan Ramesh <30742769+anonuser007@users.noreply.github.com>
2017-08-05 11:40:56 +0530

    Delete vowel.py

--- vowel.py
+++
@@ -1,8 +0,0 @@
-ch = input("Enter any character: ")
-if ch.isalpha():
- if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
-    print(ch, "is a vowel.\n")
- else:
-   print(ch, "is a consonant.\n")
-else:
-   print("Invalid!")
